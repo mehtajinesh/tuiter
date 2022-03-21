9c6fc456d7602f474bbc4d5b61926d31c71365a9
Jinesh Mehta <mehtajineshs@gmail.com>
2022-03-21 19:06:19 -0400

    removed redundant closing tags

--- src/components/Tuiter/NavigationSidebar/index.js
+++ src/components/Tuiter/NavigationSidebar/index.js
@@ -6,40 +6,40 @@ const NavigationSidebar = ({active = 'explore'}) => {
         <div>
             <div className="list-group">
                 <Link className="list-group-item" to="/">
-                    <i className="fab fa-twitter"></i>
+                    <i className="fab fa-twitter"/>
                 </Link>
                 <Link className={`list-group-item fw-bold ${active === 'home' ? 'active' : ''}`}
                    to="/tuiter/home">
-                    <i className="fa-solid fa-house-chimney"></i>
+                    <i className="fa-solid fa-house-chimney"/>
                     <span className="d-none d-xl-inline">Home</span>
                 </Link>
                 <Link className={`list-group-item fw-bold ${active === 'explore' ? 'active' : ''}`}
                    to="/tuiter/explore">
-                    <i className="fa-solid fa-hashtag"></i>
+                    <i className="fa-solid fa-hashtag"/>
                     <span className="d-none d-xl-inline">Explore</span>
                 </Link>
                 <a href="notifications.html" className="list-group-item fw-bold">
-                    <i className="fa-solid fa-bell"></i>
+                    <i className="fa-solid fa-bell"/>
                     <span className="d-none d-xl-inline">Notifications</span>
                 </a>
                 <a href="messages.html" className="list-group-item fw-bold">
-                    <i className="fa-solid fa-envelope"></i>
+                    <i className="fa-solid fa-envelope"/>
                     <span className="d-none d-xl-inline">Messages</span>
                 </a>
                 <a href="bookmarks.html" className="list-group-item fw-bold">
-                    <i className="fa-solid fa-bookmark"></i>
+                    <i className="fa-solid fa-bookmark"/>
                     <span className="d-none d-xl-inline">Bookmarks</span>
                 </a>
                 <a href="lists.html" className="list-group-item fw-bold">
-                    <i className="fa-solid fa-list"></i>
+                    <i className="fa-solid fa-list"/>
                     <span className="d-none d-xl-inline">Lists</span>
                 </a>
                 <a href="profile.html" className="list-group-item fw-bold">
-                    <i className="fa-solid fa-user"></i>
+                    <i className="fa-solid fa-user"/>
                     <span className="d-none d-xl-inline">Profile</span>
                 </a>
                 <a href="more.html" className="list-group-item fw-bold">
-                    <i className="fa-solid fa-circle"></i>
+                    <i className="fa-solid fa-circle"/>
                     <span className="d-none d-xl-inline">More</span>
                 </a>
             </div>
