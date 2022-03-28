44d43740372794c22fd6c5f79afa51e41d94589c
Jinesh Mehta <mehtajineshs@gmail.com>
2022-03-28 00:58:26 -0400

    added home src file for tuiter

---
+++ src/components/Tuiter/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ExploreScreen from "./ExploreScreen/ExploreScreen";
+const Tuiter = () => {
+    return(
+        <>
+            <ExploreScreen />
+        </>
+    )
+};
+export default Tuiter;
